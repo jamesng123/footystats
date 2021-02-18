b62882a69782813dc3f4d7dbe5a9a0cec6c476ca
James Ng <james_ng1@hotmail.co.uk>
2021-02-18 08:01:32 +0000

    Fixing how the data is cleaned

--- cleandata.py
+++ cleandata.py
@@ -12,32 +12,37 @@ def clean_data(match):
     data.set_index("player", inplace=True)
     del data["shirtnumber"]
     del data["nationality"]
-    del data["age"]
+    del data["Unnamed: 0"]
     return data
 
 
 def out_to_csv(team, match):
-    data = clean_data(f"{team}/{match}")
-    data.to_csv(f"{team}/cleaned_{match}")
+    data = clean_data(f"data/{team}/{match}")
+    data.to_csv(f"data/{team}/cleaned_{match}")
 
 
-teams = os.listdir()
-teams = [
-    i for i in teams if "." not in i and i != "footystatsenv"
+data = os.listdir()
+data = [
+    i for i in data if "." not in i and i != "footystatsenv" and i != "TODO"
 ]  # Don't want to pick up any files, just the team folders
 
-for team in teams:
-    matches = os.listdir(team)
-    for match in matches:
-        # print(f'{team}/{match}')
-        try:
-            out_to_csv(team, match)
-        except KeyError:
-            pass  # We have already cleaned this file...
-            print(team, match)
+for i in data:
+    teams = os.listdir(i)
+    for team in teams:
+        matches = os.listdir(f'{i}/{team}')
+        for match in matches:
+            # print(f'{team}/{match}')
+            try:
+                out_to_csv(team, match)
+            except KeyError:
+                pass  # We have already cleaned this file...
+                print(team, match)
 
-for team in teams:
-    matches = os.listdir(team)
-    for match in matches:
-        if "cleaned" not in match:
-            os.remove(f"{team}/{match}")
+for i in data:
+    teams = os.listdir(i)
+    for team in teams:
+        matches = os.listdir(f'{i}/{team}')
+        for match in matches:
+            print(team, match)
+            if "cleaned" not in match:
+                os.remove(f"{i}/{team}/{match}")
