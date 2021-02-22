c329800aee7b813309b96765a7adb22c466df9c2
James Ng <james_ng1@hotmail.co.uk>
2021-02-22 22:58:35 +0000

    adding to main script

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
 import fbrefscraper
 import cleandata
 
-# Need to write a function to get these links programatically
-premier_league_teams = [
-    "https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats",
-    "https://fbref.com/en/squads/19538871/Manchester-United-Stats",
-    "https://fbref.com/en/squads/18bb7c10/Arsenal-Stats",
-    "https://fbref.com/en/squads/1df6b87e/Sheffield-United-Stats",
-    "https://fbref.com/en/squads/60c6b05f/West-Bromwich-Albion-Stats",
-    "https://fbref.com/en/squads/fd962109/Fulham-Stats",
-    "https://fbref.com/en/squads/943e8050/Burnley-Stats",
-    "https://fbref.com/en/squads/b2b47a98/Newcastle-United-Stats",
-    "https://fbref.com/en/squads/d07537b9/Brighton-and-Hove-Albion-Stats",
-    "https://fbref.com/en/squads/8cec06e1/Wolverhampton-Wanderers-Stats",
-    "https://fbref.com/en/squads/47c64c55/Crystal-Palace-Stats",
-    "https://fbref.com/en/squads/33c895d4/Southampton-Stats",
-    "https://fbref.com/en/squads/5bfb9659/Leeds-United-Stats",
-    "https://fbref.com/en/squads/8602292d/Aston-Villa-Stats",
-    "https://fbref.com/en/squads/361ca564/Tottenham-Hotspur-Stats",
-    "https://fbref.com/en/squads/d3fd31cc/Everton-Stats",
-    "https://fbref.com/en/squads/7c21e445/West-Ham-United-Stats",
-    "https://fbref.com/en/squads/cff3d9bb/Chelsea-Stats",
-    "https://fbref.com/en/squads/822bd0ba/Liverpool-Stats",
-    "https://fbref.com/en/squads/a2d435b3/Leicester-City-Stats",
-]
+def get_team_list(url):
+    return fbrefscraper.get_comp_teams(url)
 
-#Need to introduce the cleaning process within here too... still need to figure this out
+def main(year, team_list):
+    fbrefscraper.main(year, team_list)
+    # cleandata.clean()
 
-def main(team_list):
-    fbrefscraper.main(team_list)
-    cleandata.clean()
+# Data is only from 17/18 onwards
 
-main(premier_league_teams)
+main("18_19", get_team_list('https://fbref.com/en/comps/9/1889/2018-2019-Premier-League-Stats'))
